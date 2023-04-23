4e9a2d273c5029a237a17813b470e4f0c5c11eae
Manoj-t <teluguntlamanoj@gmail.com>
2023-04-22 19:54:36 -0500

    Added security filter chain for the roles

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,12 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -32,5 +35,29 @@ public class DemoSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees/").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees/").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+                                            );
+
+        // use HTTP Basic authentication
+        httpSecurity.httpBasic();
+
+        // disable Cross site Request Forgery (CSRF)
+        // in general, not required for Stateless REST APIs that use POST, PUT, GET, DELETE or/and PATCH
+        httpSecurity.csrf().disable();
+
+        return httpSecurity.build();
+
+    }
+
 
 }
